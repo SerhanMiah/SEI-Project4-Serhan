257b3693649c9a89bfc020941500d7c0b03bd04c
S.Miah <serhanmiah@icloud.com>
2023-01-08 17:28:59 +0000

    updating settings

--- project/settings.py
+++ project/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import django_on_heroku
 import os
+import django_on_heroku
+
 
 from pathlib import Path
 
